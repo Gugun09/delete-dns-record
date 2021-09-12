f6754db4e45a64e28e1884279b15aeb747a7ab0c
Gugun <54890927+Gugun09@users.noreply.github.com>
2021-09-12 17:17:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 EMAIL=queen-ssh.my.id
 KEY=25dcbdfd2aa42f3b0d447c897fd989ce05895
-ZONE_ID=2222222222222222222222222
+ZONE_ID=827c3471b4f1734d19f1315add1fb306
 
 curl -sLX GET https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?per_page=500 \
      -H "X-Auth-Email: ${EMAIL}" \
