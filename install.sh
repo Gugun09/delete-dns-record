e02a5e5526947e375f544830abaf8dfed98256ea
Gugun <54890927+Gugun09@users.noreply.github.com>
2023-04-04 23:49:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-EMAIL=queen-ssh.my.id
-KEY=25dcbdfd2aa42f3b0d447c897fd989ce05895
-ZONE_ID=827c3471b4f1734d19f1315add1fb306
+EMAIL="queen-ssh.my.id"
+KEY="25dcbdfd2aa42f3b0d447c897fd989ce05895"
+ZONE_ID="827c3471b4f1734d19f1315add1fb306"
 
-curl -sLX GET https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?per_page=500 \
+curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?per_page=500" \
      -H "X-Auth-Email: ${EMAIL}" \
      -H "X-Auth-Key: ${KEY}" \
-     -H "Content-Type: application/json" | jq .result[].id |  tr -d '"' | (
+     -H "Content-Type: application/json" | jq -r '.result[] | .id' | (
   while read id; do
-    curl -sLX DELETE https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${id} \
+    curl -sLX DELETE "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${id}" \
       -H "X-Auth-Email: ${EMAIL}" \
       -H "X-Auth-Key: ${KEY}" \
-      -H "Content-Type: application/json"
+      -H "Content-Type: application/json" -o /dev/null
+    echo "DNS record with ID $id has been deleted."
   done
-  )
+)
